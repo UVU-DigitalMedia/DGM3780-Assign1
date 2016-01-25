59867a686764e6110937ffd5f51efe38b05a1a87
Thor Anderson <ta.anderson@gmail.com>
2016-01-25 10:43:47 -0700

    Second attempt

--- aboutme.js
+++ aboutme.js
@@ -1,8 +1,10 @@
 
-//Construct an object with the following properties: name(String), age(number), gender(String), bio(String, longer than 140 chars)
+//Construct an object with the following properties: name(String), 
+//age(number), gender(String), bio(String, longer than 140 chars)
 exports.student = {
     name: 'Thor Anderson',
     age: 49,
-    gender: 'Male'
-
+    gender: 'Male',
+    interests: ['web development', 'Corvairs', 'Digital Media'],
+    bio: 'This is the lengthy bio for Thor Anderson.  It must be at least 140 characters long, so I am continuing to type without really accomplishing anything'
 }
