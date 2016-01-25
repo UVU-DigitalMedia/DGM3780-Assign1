56728fd4a15bda0557c6a392af9d6b3c3c7b7b70
badicalness <brettrick@Bretts-Air.ad.uvu.edu>
2016-01-25 11:14:18 -0700

    I finished the assignment.

--- aboutme.js
+++ aboutme.js
@@ -2,10 +2,10 @@
 //Construct an object with the following properties: name(String), age(number), gender(String), bio(String, longer than 140 chars)
 exports.student = {
 	name: 'Erin Griffeth',
-	age: '26',
+	age: 26,
 	gender: 'Female',
-	interests: 'Reading, Dancing, Cooking',
-	bio: 'Here is a bio of me. Bio bio me me. When I put the word bio you expect a bio, so here it is... a bio.'
+	interests: ['Reading','Dancing','Cooking'],
+	bio: 'Here is a bio of me. Bio bio me me. When I put the word bio you expect a bio, so here it is... a bio. this bio has to at least be 140 characters. witht hat much information you can expect an amazing bio, but bios are just so.'
 }
 
 
