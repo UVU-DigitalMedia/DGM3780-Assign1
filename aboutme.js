dd4581e8be2473e7c351961603f8dba582c6b3f1
badicalness <brettrick@Bretts-Air.ad.uvu.edu>
2016-01-25 10:58:51 -0700

    here is my commit message

--- aboutme.js
+++ aboutme.js
@@ -1,5 +1,11 @@
 
 //Construct an object with the following properties: name(String), age(number), gender(String), bio(String, longer than 140 chars)
 exports.student = {
-
+	name: 'Erin Griffeth',
+	age: '26',
+	gender: 'Female',
+	interests: 'Reading, Dancing, Cooking',
+	bio: 'Here is a bio of me. Bio bio me me. When I put the word bio you expect a bio, so here it is... a bio.'
 }
+
+
