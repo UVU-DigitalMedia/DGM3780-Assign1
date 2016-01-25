c11b496bcf72f38c0ff62aed9f2a461704f92274
DUSTIN ROBERTSON <dustinuriah@gmail.com>
2016-01-25 10:55:29 -0700

    Bio updated Post career

--- aboutme.js
+++ aboutme.js
@@ -5,6 +5,6 @@ exports.student = {
     age: 27,
     interests: ['web', 'bball', 'film'],
     gender: 'male',
-    bio: 'the most famous quarterback in the history of the San Francisco 49ers, having led the team to four Super Bowl victories in the 1980s. Joe Montana joined the San Francisco 49ers in 1979 after playing college football at the University of Notre Dame. Considered something of a long shot, Montana wasnt drafted by the 49ers until the third round. He ended up as the teams starting quarterback and took the 49ers to four Super Bowl victories: in 1982, 1985, 1989 and 1990.',
+    bio: 'the most famous quarterback in the history of the San Francisco 49ers, having led the team to four Super Bowl victories in the 1980s. Joe Montana joined the San Francisco 49ers in 1979 after playing college football at the University of Notre Dame. Considered something of a long shot, Montana wasnt drafted by the 49ers until the third round. He ended up as the teams starting quarterback and took the 49ers to four Super Bowl victories: in 1982, 1985, 1989 and 1990. He is also in Papa Johns commercials.',
     
 };
