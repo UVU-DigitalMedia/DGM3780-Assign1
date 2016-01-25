fbfdce980dda732897778480add5fe9452e9720e
Thor Anderson <ta.anderson@gmail.com>
2016-01-25 10:38:27 -0700

    My first attempt

--- aboutme.js
+++ aboutme.js
@@ -1,5 +1,8 @@
 
 //Construct an object with the following properties: name(String), age(number), gender(String), bio(String, longer than 140 chars)
 exports.student = {
+    name: 'Thor Anderson',
+    age: 49,
+    gender: 'Male'
 
 }
