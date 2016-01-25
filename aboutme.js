ae6543c4b01e00abdbaaf075603b71548780b0d3
Braydon Powell <thetic1@gmail.com>
2016-01-25 10:39:51 -0700

    Corrected spelling on interests array, passing tests now.

--- aboutme.js
+++ aboutme.js
@@ -1,5 +1,5 @@
 
 //Construct an object with the following properties: name(String), age(number), gender(String), bio(String, longer than 140 chars)
-exports.student = { name: 'Braydon Powell', age: 29, gender: 'male', intrests: ['Cool Things', 'Web Development', 'Other stuff'], bio: 'A guy in class at uvu which is a school which is in orem which is in Utah. He is studying web frameworks taught by the mighty Thor Anderson and the amazing Landon Conover.'
+exports.student = { name: 'Braydon Powell', age: 29, gender: 'male', interests: ['Cool Things', 'Web Development', 'Other stuff'], bio: 'A guy in class at uvu which is a school which is in orem which is in Utah. He is studying web frameworks taught by the mighty Thor Anderson and the amazing Landon Conover.'
 
 }
