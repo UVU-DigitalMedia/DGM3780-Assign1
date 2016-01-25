64b945d4794f8d0dc84bee0d039b3539e00e8126
jeffcarbine <jeffcarbine@gmail.com>
2016-01-25 10:25:48 -0700

    Did the assignment.
    Look at me, I did the assignment!

--- aboutme.js
+++ aboutme.js
@@ -1,5 +1,13 @@
 
 //Construct an object with the following properties: name(String), age(number), gender(String), bio(String, longer than 140 chars)
 exports.student = {
-
-}
+  name : 'Phillip J. Fry',
+  gender : 'Not Female',
+  age: 1020,
+  interests: [
+    'shiny',
+    'metal',
+    'daffodil',
+  ],
+  bio: "My fellow fish monsters, far be it for me to question your stupid civilization or its dumb customs, but is squeezing each other's brains out with a giant nutcracker really going to solve anything?",
+};
