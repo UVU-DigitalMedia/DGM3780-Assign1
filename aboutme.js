3ad3b1116f9dcfcd28c5ac0c8321af5a64ee275b
Tyson Omer <tysonomer@Tysons-MBP.ad.uvu.edu>
2016-01-25 11:18:21 -0700

    Tyson's Changes

--- aboutme.js
+++ aboutme.js
@@ -1,5 +1,10 @@
 
 //Construct an object with the following properties: name(String), age(number), gender(String), bio(String, longer than 140 chars)
 exports.student = {
+Name:'Tyson Omer',
+Gender: 'Male',
+Age: '36',
+Interests:['Sky Diving', 'Hot Chocolate Drinking', 'Free Climbing'],
+Bio:'Father of 2 survivor of many enjoyable near death occorances streaming from the Comma from falling from the sky and not being able to open and back up shoot in the proper distance from the ground'
 
 }
