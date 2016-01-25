e299113190502b57c092a124ae2d44d340c829fa
Tyson Omer <tysonomer@Tysons-MBP.ad.uvu.edu>
2016-01-25 11:30:41 -0700

    Tyson

--- aboutme.js
+++ aboutme.js
@@ -5,6 +5,6 @@ Name:'Tyson Omer',
 Gender: 'Male',
 Age: '36',
 Interests:['Sky Diving', 'Hot Chocolate Drinking', 'Free Climbing'],
-Bio:'Father of 2 survivor of many enjoyable near death occorances streaming from the Comma from falling from the sky and not being able to open and back up shoot in the proper distance from the ground'
+Bio:'Father of 2 survivor of many enjoyable near death occorances streaming from the Comma from falling from the sky and not being able to open and back up shoot in the proper distance from the ground Far far away in a galaxy not that far away I was learning an amzaing things'
 
 }
