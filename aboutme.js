faafbf03f2c480712edb4adc1d2f220d934de435
jwabbitt <jrabbitt1110@gmail.com>
2016-02-18 18:14:30 -0700

    Fixed the format of my code by using single quotes and better spacing between colons

--- aboutme.js
+++ aboutme.js
@@ -1,10 +1,10 @@
 
 //Construct an object with the following properties: name(String), age(number), gender(String), bio(String, longer than 140 chars)
 exports.student = {
-  "name": "Jason Rabbitt",
-  "age": 27,
-  "gender": "male",
-  "interests": ["coding", "video games", "movies"],
-  "bio": "I first came to UVU thinking I wanted to graduate with a degree in Physics.  I decided that research and stuff wasn't for me, so I decided to take a more practical approach with Engineering.  I soon found out that I absolutely loved my CS classes, so I changed my major to computer science.  I was a bit overwhelemed at the beginning of a semester, so I switched to Digital Media with an emphasis in Internet Technologies.  I love coding!"
+  'name' : 'Jason Rabbitt',
+  'age' : 27,
+  'gender' : 'male',
+  'interests' : ['coding', 'video games', 'movies'],
+  'bio' : 'I first came to UVU thinking I wanted to graduate with a degree in Physics.  I decided that research and stuff wasn\'t for me, so I decided to take a more practical approach with Engineering.  I soon found out that I absolutely loved my CS classes, so I changed my major to computer science.  I was a bit overwhelemed at the beginning of a semester, so I switched to Digital Media with an emphasis in Internet Technologies.  I love coding!'
 
 }
