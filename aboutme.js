9de4d9996f4f11df7ffd948253a3f7ce937a70ec
Ryan Lifferth <ryanlifferth@Ryans-MBP.ad.uvu.edu>
2016-02-18 18:13:55 -0700

    Updated About Me
    Updated Name, Gender, Age, Interests and Bio

--- aboutme.js
+++ aboutme.js
@@ -1,5 +1,10 @@
 
 //Construct an object with the following properties: name(String), age(number), gender(String), bio(String, longer than 140 chars)
 exports.student = {
+    'name' : 'Ryan Lifferth',
+    'gender' : 'Male',
+    'age' : 27,
+    'interests' : ['Soccer on a', 'backpacking', 'fishing'],
+    'bio' : 'Cool Dude Loving Life that is going to school at UVU and also Managing My Costume Wigs and am starting my own buisness.  I am going to be startung a Virtual Tour business for Real Estate Agencies'
 
 }
