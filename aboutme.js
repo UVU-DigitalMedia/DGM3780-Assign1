993259c41dc5af5dd4dc6dd0c5f5bf8b24045a71
Trevor Bluth <trevorbluth@Trevors-MBP.ad.uvu.edu>
2016-02-18 18:31:11 -0700

    This may have worked

--- aboutme.js
+++ aboutme.js
@@ -1,5 +1,13 @@
 
-//Construct an object with the following properties: name(String), age(number), gender(String), bio(String, longer than 140 chars)
+//Construct an object with the following properties: 
+//name(String), age(number), gender(String), bio(String, longer than 140 chars)
 exports.student = {
-
+    'name' : 'Trevor Bluth',
+    'gender' : 'dude',
+    'age' : 25,
+    'interests' : ['eating', 'sleeping', 'video games', 'knowing how many 3 is'],
+    'bio' : 'Just another awesome dude in the class. I come, I code, I conquor, I go home. I also ramble in order to get over the minimum character count.'
 }
+
+
+
