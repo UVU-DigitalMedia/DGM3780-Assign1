93cbf17b2f4b7830d349e117b492e9bdb494a594
Jared Cooley <archcrono@gmail.com>
2016-02-18 18:18:39 -0700

    Revert "student info"
    This reverts commit 9f1ff4f1f6f8fb1b615c6b70cad73e464cda3010.

--- aboutme.js
+++ aboutme.js
@@ -1,10 +1,5 @@
 
 //Construct an object with the following properties: name(String), age(number), gender(String), bio(String, longer than 140 chars)
 exports.student = {
-	'name' : 'Jared',
-	'gender' : 'male',
-	'age' : 28,
-	'interests' : ['books','space','time'],
-	'bio' : 'I live on earth and i have lived a long time. well not that long. long enough, i can breath air and i can see food and smell water.'
 
 }
