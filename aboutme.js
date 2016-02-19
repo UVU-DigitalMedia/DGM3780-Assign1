a13308cfde24cccae5c049351ba455ec1dee7573
hbeckmann <hunterbeckmann@gmail.com>
2016-02-18 17:53:31 -0700

    Finished the assignment

--- aboutme.js
+++ aboutme.js
@@ -2,4 +2,13 @@
 //Construct an object with the following properties: name(String), age(number), gender(String), bio(String, longer than 140 chars)
 exports.student = {
 
+
+  name: "hunter",
+  gender: 'male',
+  age: 22,
+  interests: ['Rock Climbing', 'Gaming', 'Snowboarding'],
+  bio: 'My name is Hunter Beckmann and I have lived in Orem for my entire life. I enjoy technology and computers and have been using them for a large portion of my life. This is a bio and hopefully it is larger than 140 characters, Im not sure yet but Im going to contineu typing until I feel I have hit over 140 characters. I think we might be there now.'
+
+
+
 }
