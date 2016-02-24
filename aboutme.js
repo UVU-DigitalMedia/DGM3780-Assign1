9798e92a5eaaf37ff80d41ff2832882d50e4fe4c
Lorin Steel <lorinsteel@gmail.com>
2016-02-23 17:18:51 -0700

    Committing all the things!

--- aboutme.js
+++ aboutme.js
@@ -9,5 +9,6 @@ exports.student = {
     'Gaming',
     'Skiing',
     'Hanging Out'
-  ]
+  ],
+  bio: 'Lorin Steel is a husband and father of 3 girls. He currently works full time at 97th Floor as a Front End Developer and attends school full time at Utah Valley University. He\'s currently working on an assignment learning GIT. Give him money. And food. Don\'t forget the food.'
 }
