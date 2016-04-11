425b697a627bbd96d8b08aadf9fc9455cf67c90c
badicalness <badicalness@gmail.com>
2016-04-11 13:56:24 -0600

    Forking/Commit excersize, added more content.

--- aboutme.js
+++ aboutme.js
@@ -4,6 +4,8 @@ exports.student = {
 	name: 'Erin Griffeth',
 	age: 26,
 	gender: 'Female',
+    sign: 'Libra'
+    favorite movies: ['Star Trek', 'A Cinderella Story', 'Emma']
 	interests: ['Reading','Dancing','Cooking'],
 	bio: 'Here is a bio of me. Bio bio me me. When I put the word bio you expect a bio, so here it is... a bio. this bio has to at least be 140 characters. witht hat much information you can expect an amazing bio, but bios are just so.'
 }
